2d18b32a95d5352bfc7b7b70d98d4889bd99869a
Tushar <tusharbedi7373@hotmail.com>
2018-07-26 21:29:16 +0700

    added loading animation at header

--- bomberman/src/router.js
+++ bomberman/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Pending from './views/Pending.vue'
+import Loading from './views/Loading.vue'
 import Game from './views/Game.vue'
 
 Vue.use(Router)
@@ -14,9 +14,9 @@ export default new Router({
       component: Home
     },
     {
-      path: '/pending',
-      name: 'pending',
-      component: Pending
+      path: '/loading',
+      name: 'loading',
+      component: Loading
     },
     {
       path: '/game',
---
+++ bomberman/src/views/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <LoadingHeader> </LoadingHeader>
+  </div>
+
+</template>
+
+<script>
+import LoadingHeader from '@/components/LoadingHeader.vue'
+export default {
+    components: {
+      LoadingHeader
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
