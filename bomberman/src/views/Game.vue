8be09b9a692bcca46ee4ed0399998c5e4af289de
adrowicaksono <adrowicaksono@gmail.com>
2018-07-26 19:56:18 +0700

    add style on fonts

---
+++ bomberman/src/views/Game.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button v-on:click="fontSize++">
+             Increase font size
+        </button>
+        <button v-on:click="fontSize--">
+            Decrease font size
+        </button>
+
+        <p v-bind:style="{ fontSize: fontSize + 'px' }">
+            {{ fontSize }}
+        </p>
+
+    </div>
+</template>
+
+<script>
+export default {
+   data() {
+  return {
+    fontSize: 10
+  }
+}
+
+}
+</script>
